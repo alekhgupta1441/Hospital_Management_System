4770bb294868fff37f04137ffb54c3e555a99b26
Anish Aggarwal <anish17122000@gmail.com>
2021-04-21 16:22:36 +0530

    added allowed host

--- web-UI/hospitalmanagement/settings.py
+++ web-UI/hospitalmanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','hospitalmanagement1712.herokuapp.com']
 
 
 # Application definition
