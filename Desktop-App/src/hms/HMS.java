5c256250dc5646e94cfc817b243d40e7f412d1c3
alekhgupta1441 <alekhgupta1441@gmail.com>
2021-04-25 00:17:10 +0530

    Added HMS Desktop Application Source Code

---
+++ Desktop-App/src/hms/HMS.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hms;
+
+/**
+ *
+ * @author yoga
+ */
+public class HMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public HMS()
+    {
+        new page12();
+        
+        
+        
+        
+    }
+    
+    
+    
+    public static void main(String[] args) {
+       
+        
+        HMS a=new HMS();
+        
+    }
+    
+}
